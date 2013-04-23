b38dad24236946643e8b148d7d65234798392518
Mark Ootes <markootes@MacBook-Pro-van-Mark.local>
2013-04-23 11:54:53 +0200

    add some structure in the app

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+var APP = APP || {};
+
+// anonymous function
+(function(window, document){
+
+	// data here;
+	APP.data = [];
+
+	APP.controller = {
+		init: function(){
+
+		}
+	};
+
+	APP.states = {};
+	APP.pages = {};
+	APP.utils = {};
+
+})(window, document);
