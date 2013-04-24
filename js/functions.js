dac0b7e2bb27f0eba822a1d7b14f9a17c6f7b35d
Mark Ootes <markootes@001.223.028.145.hva.nl>
2013-04-24 14:09:59 +0200

    added functionality

--- js/functions.js
+++ js/functions.js
@@ -64,6 +64,8 @@ var APP = APP || {};
 			// Event delegation, for efficient event handling
 			var el = document.querySelector("body");
 			Gator(el).on('click', function(e) {
+
+				console.log(e.target);
 				//e.preventDefault();
 			    // e.target retrieves clicked element 
 			});
@@ -149,12 +151,12 @@ var APP = APP || {};
 
 	APP.map = {
 		init: function(){
-			var map = L.map('map').setView([51.505, -0.09], 13);
+			this.mapView = L.map('map').setView([51.505, -0.09], 13);
 
 			// add an OpenStreetMap tile layer
 			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			}).addTo(map);
+			}).addTo(this.mapView);
 		}
 	}
 
