a6478e20da26bcc1fd4a4b987448f0a3f3704678
Mark Ootes <markootes@MacBook-Pro-van-Mark.local>
2013-04-24 08:10:12 +0200

    added functionality

--- js/functions.js
+++ js/functions.js
@@ -4,31 +4,97 @@ var APP = APP || {};
 (function(window, document){
 
 	// data here;
-	APP.data = [
-		{
-			title: "Café",
-			lat: 25452425252,
-			lng: 32554252455,
-		},
-		{
-			title: "Voetbalvereniging",
-			lat: 25452425252,
-			lng: 32554252455,
+	APP.data = { 
+		cafe : {
+			loc: {
+				lat: 25452425252,
+				lng: 32554252455
+			},
+			image: "images/thumb.png",
+			info: "Lorem ipsum dolar sit amet"
 		}
-	];
+	}
 
 	APP.controller = {
-		init: function(){
+		// Use regular expression for string matching (gi == global search, ignore case)
+		isAndroid : (/android/gi).test(navigator.appVersion),
+		isIDevice : (/iphone|ipad/gi).test(navigator.appVersion),
 
+		init: function () {
+			// Init page states			
+			APP.states.init();
+
+			// Hide addressbar on mobile/tablet
+			APP.utils.hideAddressBar();
+			
+			Gator(window).on('orientationchange', function(e) {
+			    APP.utils.hideAddressBar();
+			});
+
+			// Event delegation, for efficient event handling
+			var el = document.querySelector("body");
+			Gator(el).on('click', function(e) {
+				e.preventDefault();
+			    // e.target retrieves clicked element 
+			});
 		}
 	};
 
-	APP.states = {};
+	APP.states = {
+		init: function () {
+			var routes = {
+		        '/locations': APP.pages.locations,
+		        '/locations/:locId': APP.pages.location
+		      };
 
-	APP.pages = {};
+			var router = Router(routes);
+      		router.init();
+		},
 
-	APP.utils = {};
-	
+		getPage: function (page, id) {
+
+            var route = window.location.hash.slice(2),
+            	id = id || false,
+                sections = document.querySelectorAll('section[data-role=page]'),
+                section = document.querySelectorAll('#'+page+']')[0];
+            
+            if (section) {
+            	for (var i=0; i < sections.length; i++){
+            		sections[i].classList.remove('show');
+            	}
+            	section.classList.add('show');
+            }
+
+		}
+
+	};
+
+	APP.pages = {
+		locations: function () {
+			APP.states.getPage('list');
+		},
+		location: function(locId){
+			APP.states.getPage('detail', locId);
+		}
+	};
+
+	APP.utils = {
+		// Source: https://gist.github.com/mhammonds/1190492#file-hidemobileaddressbar-js
+		hideAddressBar: function () {
+			if(!window.location.hash) {
+			    if(document.height < window.outerHeight) {
+			          document.body.style.height = (window.outerHeight + 50) + 'px';
+			    }
+			 
+			    setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+			}
+		}
+	};
+
+
 	APP.map = {};
+
+
+	onDomReady( APP.controller.init );
 
 })(window, document);
